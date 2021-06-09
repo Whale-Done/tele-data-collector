dce78f1bda91dc4ea29e875ff809d9399c2895ec
isbobby <bobbyclex8@gmail.com>
2021-06-10 02:28:50 +0800

    Added nested query for user input

--- tracker_app/main/routes.py
+++ tracker_app/main/routes.py
@@ -1,6 +1,4 @@
-import json
-from flask import Blueprint, render_template, url_for, redirect, request, flash
-from datetime import datetime, timedelta
+from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
@@ -8,3 +6,8 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 def home():
     return "Home"
+
+
+@main.route("/viewdata")
+def view_data():
+    return "DataPage"
