57a0b3bbec8bfab784b4c28b13e929c0a033a3ec
isbobby <bobbyclex8@gmail.com>
2021-06-11 02:17:41 +0800

    redis client connection attempt

--- tracker_app/__init__.py
+++ tracker_app/__init__.py
@@ -1,4 +1,6 @@
 import os
+from redis import Redis
+
 
 from flask import Flask
 from flask_redis import FlaskRedis
@@ -8,7 +10,12 @@ from tracker_app.config import Config
 from tracker_app.main.routes import main
 
 app = Flask(__name__)
-redis_client = FlaskRedis(app)
+
+# for deploy
+redis_client = Redis(host="ec2-54-221-249-45.compute-1.amazonaws.com", port=14060, db=0, password='p22272393a04e83e3c4b7cbe21faa98ac3d93cf57e55203ffa43eef0f3a2d5113')
+
+# for local debug
+# redis_client = FlaskRedis(app)
 db = SQLAlchemy(app)
 
 app.config.from_object(Config)
