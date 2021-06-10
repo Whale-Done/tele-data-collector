ac6b8febce0be669599fea324ceb38bbf0b4ab7f
isbobby <bobbyclex8@gmail.com>
2021-06-11 02:03:41 +0800

    Edited deploy webhook url

--- tracker_bot/credentials.py
+++ tracker_bot/credentials.py
@@ -2,5 +2,7 @@ bot_token = '1805332688:AAFPnLTJKzpPnDFUxD1EYx67EwWk8XZ0Kvc'
 bot_user_name = "Tracker"
 # URL = "https://money-goose-tele-server.herokuapp.com/"
 # URL = "https://e1cb37253fc6.ngrok.io/"
-URL = "https://e1cb37253fc6.ngrok.io/"
+
+# URL = "https://e1cb37253fc6.ngrok.io/"
+URL = "https://tele-whalemoney.herokuapp.com/"
 reset_key = 'resetkey2H782F9H08F2N9UBPVSHDBDJASJDONsSDHS90D'
