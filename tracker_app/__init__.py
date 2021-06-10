b3ad54767ddcdadabbbf0630301b58343c896e70
isbobby <bobbyclex8@gmail.com>
2021-06-11 02:28:39 +0800

    Fixed deploy redis config

--- tracker_app/__init__.py
+++ tracker_app/__init__.py
@@ -13,7 +13,6 @@ from tracker_app.main.routes import main
 app = Flask(__name__)
 
 # for deploy
-# redis_client = Redis(host="ec2-54-221-249-45.compute-1.amazonaws.com", port=14060, db=0, password='p22272393a04e83e3c4b7cbe21faa98ac3d93cf57e55203ffa43eef0f3a2d5113', ssl=True)
 REDIS_DEFAULT_CONNECTION_POOL = redis.ConnectionPool.from_url(os.getenv('REDIS_URL', 'redis://localhost:6379/'))
 redis_client = Redis(connection_pool=REDIS_DEFAULT_CONNECTION_POOL)
 
