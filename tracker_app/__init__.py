4134592045496aa9b8df784c6545dd7bdfac389a
isbobby <bobbyclex8@gmail.com>
2021-06-11 01:46:27 +0800

    Edited configs

--- tracker_app/__init__.py
+++ tracker_app/__init__.py
@@ -1,16 +1,19 @@
 import os
 
 from flask import Flask
+from flask_redis import FlaskRedis
+from flask_sqlalchemy import SQLAlchemy
+
 from tracker_app.config import Config
+from tracker_app.main.routes import main
+
+app = Flask(__name__)
+redis_client = FlaskRedis(app)
+db = SQLAlchemy(app)
+
+app.config.from_object(Config)
+
+# import the routes as classes and register these blueprints into the flask app
 
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
-
-    # import the routes as classes and register these blueprints into the flask app
-    from tracker_app.main.routes import main
-
-    app.register_blueprint(main)
-
-    return app
+app.register_blueprint(main)
--- tracker_app/config.py
+++ tracker_app/config.py
@@ -7,8 +7,8 @@ class Config:
     USER_ENABLE_USERNAME = True  # Enable username authentication
     USER_REQUIRE_RETYPE_PASSWORD = False
     # for deploy
-    # REDIS_URL = "redis://leipetushood:1234567890@localhost:6379"
-    # SQLALCHEMY_DATABASE_URI = ''
+    REDIS_URL = "redis://:p22272393a04e83e3c4b7cbe21faa98ac3d93cf57e55203ffa43eef0f3a2d5113@ec2-54-221-249-45.compute-1.amazonaws.com:14060"
+    SQLALCHEMY_DATABASE_URI = 'postgres://uhnjiuiznnyqsb:e0118cc80785491f69f8e54cf16d7fef8837745aa5b5b19d93c0a0e8d0adf251@ec2-3-224-7-166.compute-1.amazonaws.com:5432/d38r95t4r9dm9a'
     # for debug
-    REDIS_URL = "redis://leipetushood:1234567890@localhost:6379"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://leipetushood:1234567890@localhost:5432/whalemoney'
+    # REDIS_URL = "redis://leipetushood:1234567890@localhost:6379"
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://leipetushood:1234567890@localhost:5432/whalemoney'
