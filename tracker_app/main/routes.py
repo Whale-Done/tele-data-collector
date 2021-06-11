f708f913dde9aa8d0c955e195a05d1ceb70b8714
nicholaskinexcs <nicholas@kinexcs.com>
2021-06-11 12:43:58 +0800

    Added dataview page resources and basic binding

--- tracker_app/main/routes.py
+++ tracker_app/main/routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 main = Blueprint('main', __name__)
+
 
 @main.route("/")
 @main.route("/home")
@@ -9,4 +11,46 @@ def home():
 
 @main.route("/viewdata")
 def view_data():
-    return "DataPage"
+    amount_list = [4.5, 23, 3, 4, 5]
+    expense_detail_list = [
+        {
+            'category': 'food',
+            'description': 'lunch',
+            'purchase_type': 'impulsive, but need?',
+            'expense_time': '2021-06-12 12:00:31',
+            'submit_time': '2021-06-12 12:30:00'
+        },
+
+        {
+            'category': 'food',
+            'description': 'lunch',
+            'purchase_type': 'impulsive, but need?',
+            'expense_time': '2021-06-12 12:00:31',
+            'submit_time': '2021-06-12 12:30:00'
+        },
+
+        {
+            'category': 'food',
+            'description': 'lunch',
+            'purchase_type': 'impulsive, but need?',
+            'expense_time': '2021-06-12 12:00:31',
+            'submit_time': '2021-06-12 12:30:00'
+        },
+
+        {
+            'category': 'food',
+            'description': 'lunch',
+            'purchase_type': 'impulsive, but need?',
+            'expense_time': '2021-06-12 12:00:31',
+            'submit_time': '2021-06-12 12:30:00'
+        },
+
+        {
+            'category': 'food',
+            'description': 'lunch',
+            'purchase_type': 'impulsive, but need?',
+            'expense_time': '2021-06-12 12:00:31',
+            'submit_time': '2021-06-12 12:30:00'
+        },
+    ]
+    return render_template('dataview.html', amount_list=amount_list, expense_detail_list=expense_detail_list)
